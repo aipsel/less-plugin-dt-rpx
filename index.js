eba984934110c795e1726052fb326e3c10751cec
Yeo Yang <yuyaoyang09@gmail.com>
2018-12-31 02:11:54 +0800

    add array value support and retain the original unit.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+//
+// Copyright (c) 2017 北京大愚智能科技有限公司
+//
+
+function toRpx(node) {
+    if (Array.isArray(node.value)) {
+        return node.value.map(toRpx).join(' ');
+    } else {
+        if (node.unit && node.unit.backupUnit) {
+            return node.value + node.unit.backupUnit;
+        } else {
+            return node.value + 'rpx';
+        }
+    }
+}
+
 module.exports = {
     install: function (less, pluginManager, functions) {
-        functions.add('rpx', function (size) {
-            return size.value + 'rpx';
-        });
+        functions.add('rpx', toRpx);
     },
 };
